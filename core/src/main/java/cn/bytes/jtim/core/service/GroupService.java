0d65057d86e8410c789e1a7d7bc0f4d7cbeee385
江浩 <jh_vip_1024@163.com>
2020-03-09 10:58:16 +0800

    'feat:定义部分处理接口'

--- core/src/main/java/cn/bytes/jtim/core/service/GroupService.java
+++ core/src/main/java/cn/bytes/jtim/core/service/GroupService.java
@@ -10,18 +10,60 @@ import cn.bytes.jtim.core.protocol.protobuf.DefineResponse;
  */
 public interface GroupService {
 
+    /**
+     * 创建群信息
+     *
+     * @param createRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse create(cn.bytes.jtim.core.protocol.protobuf.CreateRequest createRequest);
 
+    /**
+     * 删除群信息
+     *
+     * @param removeRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse remove(cn.bytes.jtim.core.protocol.protobuf.RemoveRequest removeRequest);
 
+    /**
+     * 禁用群用户信息
+     *
+     * @param forbiddenRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse forbidden(cn.bytes.jtim.core.protocol.protobuf.ForbiddenRequest forbiddenRequest);
 
+    /**
+     * 取消禁用
+     *
+     * @param unForbiddenRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse unForbidden(cn.bytes.jtim.core.protocol.protobuf.UnForbiddenRequest unForbiddenRequest);
 
+    /**
+     * 加入群消息
+     *
+     * @param joinRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse join(cn.bytes.jtim.core.protocol.protobuf.JoinRequest joinRequest);
 
+    /**
+     * 离开群消息
+     *
+     * @param leaveRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse leave(cn.bytes.jtim.core.protocol.protobuf.LeaveRequest leaveRequest);
 
+    /**
+     * 踢出群
+     *
+     * @param kickRequest
+     * @return cn.bytes.jtim.core.protocol.protobuf.DefineResponse
+     **/
     DefineResponse kick(cn.bytes.jtim.core.protocol.protobuf.KickRequest kickRequest);
 
 }
